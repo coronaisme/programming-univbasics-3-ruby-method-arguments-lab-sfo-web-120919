526a1c3b9b9019f376b94ae79dfc92e00ea84b17
Nick Corona <coronaisme@gmail.com>
2019-11-27 22:30:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,12 +11,10 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language)
-  if language != "Ruby"
-    puts "Hi, my name is #{name} and I am learning to program in Ruby."
-  else
-    puts "Hi, my name is #{name} and I am learning to program in #{language}."
-  end
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+  
 
 
 
